2549334afba7f3d490a4e97db4d9e767b9b196e3
Maciej Derewiański <eco_derma@poczta.onet.pl>
2021-10-04 12:36:27 +0200

    Fix mongodb uri

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")]
   },
   env: {
-    MONGO_URI: process.env.NODE_ENV !== 'development' ? 'mongodb://localhost:27017/Athena' : "mongodb://localhost:27017/Athena",
+    MONGO_URI: process.env.NODE_ENV !== 'development' ? 'mongodb+srv://passDB1:MikaeL2304@take2.yadiv.gcp.mongodb.net/Athena' : "mongodb://localhost:27017/Athena",
     MONGODB_DB: "Athena",
     JWT_SECRET: "a0b16720-b910-4ec7-b511-0bef3e008a14"
   }
