d788b69f80f6948249d2c83f6b355174fc99dcc3
Maciej Derewiański <eco_derma@poczta.onet.pl>
2021-10-04 12:48:20 +0200

    Fix host url

--- utils/env.ts
+++ utils/env.ts
@@ -1,1 +1,1 @@
-export const server = process.env.NODE_ENV !== 'development' ? 'http://localhost:3000' : 'http://localhost:3000';
+export const server = process.env.NODE_ENV !== 'development' ? 'http://stadninaxnt.vercel.app' : 'http://localhost:3000';
