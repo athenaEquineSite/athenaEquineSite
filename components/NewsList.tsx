2621514707d1563403bd0e9cc56291b73f0c299d
Maciej Derewiański <eco_derma@poczta.onet.pl>
2021-12-14 00:38:40 +0100

    Fix newses sort

--- components/NewsList.tsx
+++ components/NewsList.tsx
@@ -2,9 +2,10 @@ import News from './News';
 import styles from '../styles/News.module.scss';
 
 const NewsList = ({posts}) => {
+    const sortedPosts = posts.reverse();
     return (
         <div className={`${styles.newsList}`}>
-        {posts.reverse().map((post) => (
+        {sortedPosts.map((post) => (
             <News key={post.postId} news={post}/>
         ))}
         </div>
