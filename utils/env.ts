022ebb88ab28237bf3da7a366de1e88fc48aebd6
Athena <athena.equine.site@gmail.com>
2022-04-05 23:25:16 +0200

    Change site name

--- utils/env.ts
+++ utils/env.ts
@@ -1,5 +1,5 @@
 
-export const server = process.env.NODE_ENV !== 'development' ? 'https://athenaequine.no' : 'http://localhost:3000';
+export const server = process.env.NODE_ENV !== 'development' ? 'https://athena-equine-site.vercel.app' : 'http://localhost:3000';
 export const jwtSecret = process.env.JWT_SECRET;
 export const googleKey = process.env.NEXT_PUBLIC_API_KEY;
 export const mongoUri = process.env.MONGO_URI;
